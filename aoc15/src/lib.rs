e261239e2e988d3d41883a5a5641b67ae4f3d402
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-07-04 00:57:29 +0500

    aoc 2015 initial

---
+++ aoc15/src/lib.rs
@@ -0,0 +1,15 @@
+use aoc_trait::AOC;
+
+pub struct AOC15;
+
+impl AOC for AOC15 {
+  const YEAR: usize = 2015;
+
+  fn last() -> String {
+    String::new()
+  }
+
+  fn all() -> Vec<String> {
+    Vec::new()
+  }
+}
