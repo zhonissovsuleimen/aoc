5abbfeed88ed4d1ef6e5167172df61a6fb1e266d
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-07-05 02:02:42 +0500

    2015 d3

--- aoc15/src/days/d3.rs
+++ aoc15/src/days/d3.rs
@@ -20,6 +20,22 @@ For example:
     ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
 */
 
+/*
+--- Part Two ---
+
+The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+    ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+    ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+    ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+*/
+
 impl Day for D3 {
   fn day(&self) -> usize {
     3
@@ -66,6 +82,47 @@ impl Day for D3 {
   }
 
   fn solution_extra(&self) -> Option<String> {
-    None
+    let Some(input) = self.input() else {
+      return None;
+    };
+
+    let mut santa = HashSet::<(i32, i32)>::new();
+    let mut robosanta = HashSet::<(i32, i32)>::new();
+    let mut santa_coords = (0, 0);
+    let mut robosanta_coords = (0, 0);
+
+    santa.insert(santa_coords);
+    robosanta.insert(robosanta_coords);
+    for (i, c) in input.chars().enumerate() {
+      let coords;
+      let person;
+
+      if i % 2 == 0 {
+        person = &mut santa;
+        coords = &mut santa_coords;
+      } else {
+        person = &mut robosanta;
+        coords = &mut robosanta_coords;
+      }
+
+      coords.0 += match c {
+        '>' => 1,
+        '<' => -1,
+        _ => 0
+      };
+
+      coords.1 += match c {
+        '^' => 1,
+        'v' => -1,
+        _ => 0
+      };
+
+      person.insert(*coords);
+    }
+
+    santa.extend(robosanta);
+
+    let result = santa.len();
+    Some(result.to_string())
   }
 }
