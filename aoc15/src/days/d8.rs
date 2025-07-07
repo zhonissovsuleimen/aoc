3ec6d4f8e18140c20685674c9604cbd664130e58
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-07-07 07:09:10 +0500

    2015 d8 extra + removed leftover printlns

--- aoc15/src/days/d8.rs
+++ aoc15/src/days/d8.rs
@@ -25,6 +25,22 @@ Disregarding the whitespace in the file, what is the number of characters of cod
 For example, given the four strings above, the total number of characters of string code (2 + 5 + 10 + 6 = 23) minus the total number of characters in memory for string values (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
 */
 
+/*
+--- Part Two ---
+
+Now, let's go the other way. In addition to finding the number of characters of code, you should now encode each code representation as a new string and find the number of characters of the new encoded representation, including the surrounding double quotes.
+
+For example:
+
+    "" encodes to "\"\"", an increase from 2 characters to 6.
+    "abc" encodes to "\"abc\"", an increase from 5 characters to 9.
+    "aaa\"aaa" encodes to "\"aaa\\\"aaa\"", an increase from 10 characters to 16.
+    "\x27" encodes to "\"\\x27\"", an increase from 6 characters to 11.
+
+Your task is to find the total number of characters to represent the newly encoded strings minus the number of characters of code in each original string literal. For example, for the strings above, the total encoded length (6 + 9 + 16 + 11 = 42) minus the characters in the original code representation (23, just like in the first part of this puzzle) is 42 - 23 = 19.
+
+*/
+
 impl Day for D8 {
   fn day(&self) -> usize {
     8
@@ -74,8 +90,6 @@ impl Day for D8 {
       }
 
       result += code_len - mem_len;
-      println!("{line}");
-      println!("{}-{}", code_len, mem_len);
     }
 
     Some(result.to_string())
@@ -85,7 +99,27 @@ impl Day for D8 {
     let Some(input) = self.input() else {
       return None;
     };
+    let mut result = 0;
 
-    None
+    for line in input.lines() {
+      let code_len = line.len();
+      let mut encoded_len = 2;
+
+      let chars = line.chars().collect::<Vec<char>>();
+      for c in chars {
+        match c {
+          '\\' | '"' => {
+            encoded_len += 2;
+          }
+          _ => {
+            encoded_len += 1;
+          }
+        }
+      }
+
+      result += encoded_len - code_len;
+    }
+
+    Some(result.to_string())
   }
 }
