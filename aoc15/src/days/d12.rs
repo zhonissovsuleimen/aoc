7af00272f890a2f6f0b76f5119df5be23105c671
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-07-09 05:44:31 +0500

    2015 d12 extra

--- aoc15/src/days/d12.rs
+++ aoc15/src/days/d12.rs
@@ -22,6 +22,20 @@ You will not encounter any strings containing numbers.
 What is the sum of all numbers in the document?
 */
 
+/*
+--- Part Two ---
+
+Uh oh - the Accounting-Elves have realized that they double-counted everything red.
+
+Ignore any object (and all of its children) which has any property with the value "red". Do this only for objects ({...}), not arrays ([...]).
+
+    [1,2,3] still has a sum of 6.
+    [1,{"c":"red","b":2},3] now has a sum of 4, because the middle object is ignored.
+    {"d":"red","e":[1,2,3,4],"f":5} now has a sum of 0, because the entire structure is ignored.
+    [1,"red",5] has a sum of 6, because "red" in an array has no effect.
+
+*/
+
 impl Day for D12 {
   fn day(&self) -> usize {
     12
@@ -53,7 +67,9 @@ impl Day for D12 {
       return None;
     };
 
-    None
+    let v: Value = serde_json::from_str(&input).unwrap();
+
+    Some(count_nums_no_red(v).to_string())
   }
 }
 
@@ -75,6 +91,42 @@ fn count_nums(json: Value) -> i32 {
       let mut sum = 0;
       for child_value in map.into_values() {
         sum += count_nums(child_value);
+      }
+      sum
+    }
+  }
+}
+
+fn count_nums_no_red(json: Value) -> i32 {
+  match json {
+    Value::Null => 0,
+    Value::Bool(_) => 0,
+    Value::Number(number) => number.as_i64().unwrap_or(0) as i32,
+    Value::String(_) => 0,
+    Value::Array(values) => {
+      let mut sum = 0;
+      for child_value in values {
+        sum += count_nums_no_red(child_value);
+      }
+
+      sum
+    }
+    Value::Object(map) => {
+      let mut sum = 0;
+      let mut contains_red = false;
+
+      for child_value in map.values() {
+        if let Value::String(string_value) = child_value {
+          if string_value == "red" {
+            contains_red = true;
+          }
+        }
+      }
+
+      if !contains_red {
+        for child_value in map.into_values() {
+          sum += count_nums_no_red(child_value);
+        }
       }
       sum
     }
