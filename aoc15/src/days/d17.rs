600ec105e2ea7c1395726260b04fdd3b1cc573ba
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-07-11 15:00:43 +0500

    2015 d7 description & wrong fn used fix

--- aoc15/src/days/d17.rs
+++ aoc15/src/days/d17.rs
@@ -2,6 +2,20 @@ use aoc_trait::Day;
 
 pub struct D17;
 
+/*
+--- Day 17: No Such Thing as Too Much ---
+
+The elves bought too much eggnog again - 150 liters this time. To fit it all into your refrigerator, you'll need to move it into smaller containers. You take an inventory of the capacities of the available containers.
+
+For example, suppose you have containers of size 20, 15, 10, 5, and 5 liters. If you need to store 25 liters, there are four ways to do it:
+
+    15 and 10
+    20 and 5 (the first 5)
+    20 and 5 (the second 5)
+    15, 5, and 5
+
+Filling all containers entirely, how many different combinations of containers can exactly fit all 150 liters of eggnog?
+*/
 impl Day for D17 {
   fn day(&self) -> usize {
     17
@@ -19,14 +33,6 @@ impl Day for D17 {
   }
 
   fn solution(&self) -> Option<String> {
-    let Some(input) = self.input() else {
-      return None;
-    };
-
-    None
-  }
-
-  fn solution_extra(&self) -> Option<String> {
     let Some(input) = self.input() else {
       return None;
     };
@@ -50,5 +56,13 @@ impl Day for D17 {
 
     let target_solution = dp_solutions[target_volume];
     Some(target_solution.to_string())
+  }
+
+  fn solution_extra(&self) -> Option<String> {
+    let Some(input) = self.input() else {
+      return None;
+    };
+
+    None
   }
 }
